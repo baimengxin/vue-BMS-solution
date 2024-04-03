d89e93f058e4338dc83fb3d40cb5006f9f3ddcbe
小白萌新 <3036403996@qq.com>
2024-04-04 02:22:07 +0800

    feat: 菜单模块头部 logo 显示

--- src/views/layout/components/Sidebar/index.vue
+++ src/views/layout/components/Sidebar/index.vue
@@ -1,12 +1,42 @@
 <script setup>
 // import SidebarMenu from '@/views/layout/components/Sidebar/SidebarMenu.vue'
+import { useMainStore } from '@/stores'
 import SidebarMenu from './SidebarMenu.vue'
+
+const store = useMainStore()
 </script>
 
 <template>
   <div class="sidebar">
-    <SidebarMenu />
+    <div class="logo-container">
+      <el-avatar
+        size="44"
+        shape="square"
+        src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png"
+      />
+      <h1 class="logo-title" v-if="store.sidebarStatus">imooc-admin</h1>
+    </div>
+
+    <el-scrollbar>
+      <SidebarMenu />
+    </el-scrollbar>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.logo-container {
+  height: 44px;
+  padding: 10px 0 22px 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .logo-title {
+    margin-left: 10px;
+    color: #fff;
+    font-weight: 600;
+    line-height: 50px;
+    font-size: 16px;
+    white-space: nowrap;
+  }
+}
+</style>
