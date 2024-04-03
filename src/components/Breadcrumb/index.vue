097adff8b52c4204cd1830d8798e5c2d8d2616d3
小白萌新 <3036403996@qq.com>
2024-04-04 04:58:59 +0800

    feat: 动态渲染面包屑和面包屑动画效果

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,11 +1,45 @@
-<script setup></script>
+<script setup>
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+
+// 生成数组结构
+const breadcrumbData = ref([])
+
+const getBreadcrumbData = () => {
+  // 当前路由的路由表 route.matched
+  breadcrumbData.value = route.matched.filter((item) => item.meta && item.meta.title)
+}
+
+watch(
+  route,
+  () => {
+    getBreadcrumbData()
+  },
+  {
+    immediate: true
+  }
+)
+
+// 点击跳转
+const router = useRouter()
+const onLinkClick = (item) => {
+  router.push(item.path)
+}
+</script>
 
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">homepage</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">promotion management</a></el-breadcrumb-item>
-    <el-breadcrumb-item>promotion list</el-breadcrumb-item>
-    <el-breadcrumb-item>promotion detail</el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
+        <!-- 不可点击 -->
+        <span v-if="index === breadcrumbData.length - 1">{{ item.meta.title }}</span>
+
+        <!-- 可点击 -->
+        <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{ item.meta.title }}</a>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
@@ -19,6 +53,16 @@
   :deep(.no-redirect) {
     color: #97a8be;
     cursor: text;
+  }
+
+  .redirect {
+    color: #666;
+    font-weight: 600;
+  }
+
+  .redirect:hover {
+    // 将来需要进行主题替换，不写死样式
+    color: var(--menu-bg);
   }
 }
 </style>
