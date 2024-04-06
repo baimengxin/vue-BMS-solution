54e90d3f2ebe121f773c08e59e4691500d7e7359
小白萌新 <3036403996@qq.com>
2024-04-06 20:15:24 +0800

    fix: 解决 element-plus 主题色变更，刷新失效问题

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-import { useMainStore } from '@/stores'
+import { useMainStore, useThemeStore } from '@/stores'
 import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 import en from 'element-plus/dist/locale/en.mjs'
 import { computed } from 'vue'
+import { generateNewStyle, writeNewStyle } from '@/utils/theme'
 
-const store = useMainStore()
-const locale = computed(() => (store.language === 'en' ? en : zhCn))
+// 国际化
+const mainStore = useMainStore()
+const locale = computed(() => (mainStore.language === 'zh' ? zhCn : en))
+
+// 主题色变更
+const themeStore = useThemeStore()
+generateNewStyle(themeStore.mainColor).then((newStyleText) => writeNewStyle(newStyleText))
 </script>
 
 <template>
