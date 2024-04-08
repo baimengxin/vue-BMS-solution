2a4e7001e9f78b2272cceae2aef2598914b2e1cf
小白萌新 <3036403996@qq.com>
2024-04-08 16:25:26 +0800

    feat: 个人中心-头像组件完成

--- src/views/profile/components/ProjectCard.vue
+++ src/views/profile/components/ProjectCard.vue
@@ -1,7 +1,17 @@
-<script setup></script>
+<script setup>
+import PanThumb from '@/components/PanThumb/index.vue'
+import { useUserStore } from '@/stores'
+
+const store = useUserStore()
+</script>
 
 <template>
-  <div class="">项目介绍</div>
+  <div class="">
+    <PanThumb :image="store.userInfo.avatar" :height="'100px'" :width="'100px'">
+      <div>Hello</div>
+      {{ store.userInfo.title }}
+    </PanThumb>
+  </div>
 </template>
 
 <style lang="scss" scoped></style>
