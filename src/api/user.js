690871caa9c5ca40c7d4d8308e9b85efcc3036af
小白萌新 <3036403996@qq.com>
2024-04-08 18:59:37 +0800

    feat: 个人中心-功能、章节、作者模块完成

--- src/api/user.js
+++ src/api/user.js
@@ -1,7 +1,19 @@
 import request from '@/utils/request'
 
+/**
+ * 功能
+ * */
 export const getFeature = () => {
   return request({
     url: '/user/feature'
+  })
+}
+
+/**
+ * 章节
+ * */
+export const getWritings = () => {
+  return request({
+    url: '/user/chapter'
   })
 }
--- src/views/profile/components/Writings.vue
+++ src/views/profile/components/Writings.vue
@@ -1,7 +1,30 @@
-<script setup></script>
+<script setup>
+import { getWritings } from '@/api/user'
+import { watchSwitchLang } from '@/utils/i18n'
+import { ref } from 'vue'
+
+const writingsData = ref([])
+const getWritingsData = async () => {
+  writingsData.value = await getWritings()
+}
+
+getWritingsData()
+watchSwitchLang(getWritingsData)
+</script>
 
 <template>
-  <div class="">章节</div>
+  <el-timeline>
+    <el-timeline-item
+      v-for="item in writingsData"
+      :key="item.id"
+      :timestamp="item.timestamp"
+      placement="top"
+    >
+      <el-card>
+        <h4>{{ item.content }}</h4>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
 </template>
 
 <style lang="scss" scoped></style>
