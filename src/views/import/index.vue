e586e743611b78d0686a8bb3d9c99caf23bf4283
小白萌新 <3036403996@qq.com>
2024-04-09 01:29:38 +0800

    feat: 文件上传-文件选择之后的数据解析处理

--- src/views/import/index.vue
+++ src/views/import/index.vue
@@ -1,12 +1,19 @@
-<template>
-  <div class="import">
-    <UploadExcel />
-  </div>
-</template>
-
 <script setup>
 import {} from 'vue'
 import UploadExcel from '@/components/UploadExcel/index.vue'
+
+/**
+ * 数据解析成功之后的回调
+ */
+const onSuccess = (excelData) => {
+  console.log(excelData)
+}
 </script>
+
+<template>
+  <div class="import">
+    <UploadExcel :onSuccess="onSuccess" />
+  </div>
+</template>
 
 <style lang="scss" scoped></style>
