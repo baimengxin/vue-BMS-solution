09dbb5c27ca7b997e9a2dee3554e80cfe948eb30
小白萌新 <3036403996@qq.com>
2024-04-12 03:45:21 +0800

    feat: 创建文章-基本布局结构完成

--- src/views/article-create/index.vue
+++ src/views/article-create/index.vue
@@ -1,9 +1,37 @@
-<template>
-  <div class="">创建文章</div>
-</template>
-
 <script setup>
-import {} from 'vue'
+import { ref } from 'vue'
+import Markdown from './components/Markdown.vue'
+import Editor from './components/Editor.vue'
+
+const title = ref('')
+const activeName = ref('markdown')
 </script>
 
-<style lang="scss" scoped></style>
+<template>
+  <div class="article-create">
+    <el-card>
+      <el-input
+        class="title-input"
+        :placeholder="$t('msg.article.titlePlaceholder')"
+        v-model="title"
+        maxlength="20"
+        clearable
+      />
+
+      <el-tabs v-model="activeName">
+        <el-tab-pane :label="$t('msg.article.markdown')" name="markdown">
+          <Markdown />
+        </el-tab-pane>
+        <el-tab-pane :label="$t('msg.article.richText')" name="editor">
+          <Editor />
+        </el-tab-pane>
+      </el-tabs>
+    </el-card>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title-input {
+  margin-bottom: 20px;
+}
+</style>
