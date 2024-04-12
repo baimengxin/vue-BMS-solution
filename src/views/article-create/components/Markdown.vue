04347cdac5264020e45b635c2566d0b35bb23410
小白萌新 <3036403996@qq.com>
2024-04-12 15:42:52 +0800

    feat: 创建文章-markdown 功能实现

--- src/views/article-create/components/Markdown.vue
+++ src/views/article-create/components/Markdown.vue
@@ -1,7 +1,58 @@
-<script setup></script>
+<script setup>
+import MkEditor from '@toast-ui/editor'
+import '@toast-ui/editor/dist/toastui-editor.css'
+import '@toast-ui/editor/dist/i18n/zh-cn'
+import { onMounted } from 'vue'
+import { useMainStore } from '@/stores'
+import { watchSwitchLang } from '@/utils/i18n'
+
+// Editor 实例
+let mkEditor
+// 处理离开页面切换语言无法获取 dom
+let el
+onMounted(() => {
+  el = document.querySelector('#markdown-box')
+  console.log(store.language)
+  initEditor()
+})
+
+const store = useMainStore()
+const initEditor = () => {
+  mkEditor = new MkEditor({
+    el,
+    height: '500px',
+    previewStyle: 'vertical',
+    language: store.language === 'zh' ? 'zh-CN' : 'en'
+  })
+
+  mkEditor.getMarkdown()
+}
+
+// 监听语言的变化
+watchSwitchLang(() => {
+  if (!el) return
+  const htmlStr = mkEditor.getHTML()
+  mkEditor.destroy()
+  initEditor()
+  mkEditor.setHTML(htmlStr)
+})
+</script>
 
 <template>
-  <div class=""></div>
+  <div class="markdown-container">
+    <!-- 渲染区 -->
+    <div id="markdown-box"></div>
+    <div class="bottom">
+      <el-button type="primary" @click="onSubmitClick">{{ $t('msg.article.commit') }}</el-button>
+    </div>
+  </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.markdown-container {
+  .bottom {
+    margin-top: 20px;
+    text-align: right;
+  }
+}
+</style>
